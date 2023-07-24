0227942c361f698aa0fc0574d64d966a833b21f0
Themwhiskers <zvika.sage@gmail.com>
2023-07-24 20:32:58 +0300

    init data

---
+++ indie-db/src/cards/components/card/pages/Cards.jsx
@@ -0,0 +1,23 @@
+const cardData = {
+    titleId: '',
+    titleName: 'Transistor',
+    titleStudio: 'Super Giant Games',
+    devTeam: [
+        'Christopher Jurney',
+        'Gavin Simon',
+        'Andrew Wang'
+    ],
+    artBy: [
+        'Jen Zee'
+    ],
+    composer: 'Darren Korb',
+    platforms: [
+        'Steam',
+        'Xbox360',
+        'Xbox One',
+        'PlayStation 4',
+    ],
+    steamPage: 'https://store.steampowered.com/app/237930/Transistor/',
+    gogPage: 'https://www.gog.com/en/game/transistor',
+    psnPage: 'https://store.playstation.com/en-us/product/UP2125-CUSA00502_00-TRANSISTORGAME00'
+}
