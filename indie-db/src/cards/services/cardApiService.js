5c3e4e9da68d04382a5e01599afe5efeea149e2e
Themwhiskers <zvika.sage@gmail.com>
2023-08-01 22:26:55 +0300

    server setup

---
+++ indie-db/src/cards/services/cardApiService.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import '../../hooks/useAxios';
+
+const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:8181';
+
+export const getCards = async () => {
+    try {
+        const { data } = await axios.get(`${apiUrl}/cards`);
+        return data;
+    } catch (error) {
+        return Promise.reject(error.message);
+    }
+}
